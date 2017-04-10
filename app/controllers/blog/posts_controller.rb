36fae505bb5e154a9a4c28410fffc89c761ecb3f
Ruben Reyes <chad@MacBook-Pro-de-chad.local>
2017-04-10 08:43:41 -0500

    Se carga barra de navegacion y foooterinicial

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -4,11 +4,11 @@ module Blog
     # GET /posts
     # GET /posts.json
     def index
-    
+
     @posts = storage.list_for(params[:page], params[:tag])
     end
 
-
+  
 
     # GET /posts/1
     # GET /posts/1.json
